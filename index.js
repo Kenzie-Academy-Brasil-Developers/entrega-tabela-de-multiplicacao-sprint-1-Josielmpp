7d34950e00fa396bdb2308e7d4b554a82d602e12
Josielmpp <josielmpp@gmail.com>
2021-08-12 14:36:51 -0400

    matriz 10x10 da tabuada finalizada

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
             // Se preferir, utilize o script em um arquivo .js separado.
             function nomeDaSuaFuncao() { // Substitua "nomeDaSuaFuncao" por um nome mais apropriado
                 // TODO: escrever dois loops aninhados para desenhar o array bidimensional e monstar o restante da tabela no console.
-                let x = [];
-                for (let i = 0; i <= 10; i++) {
-                    x[i] = [];
+            
+            
+                let tabuada = []
+                let resultado = 0
+
+                for (let i = 0; i <= 10; i++) {//cria 11 array
+                    
+                    
+                    let x = []
+                    tabuada.push(x)
+                        for(let contador = 0; contador <= 10 ; contador++){
+                            resultado = contador * i
+                            tabuada[i].splice(10,0,resultado)
+                            // console.log(contador)
+                            // console.log("i "+i+"x"+" "+contador+" = "+contador * i)
+
+                        }    
+                        
+                   
+                   
+                
                 }
-                console.table(x)
+               
+                console.table(tabuada)
             }
+            nomeDaSuaFuncao()
